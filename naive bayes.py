426e92deebdccedeb61fba2b9791d71176c88d6a
prashanth <prashanthchowdary083@gmail.com>
2026-01-06 19:16:47 +0530

    Add files via upload

---
+++ naive bayes.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  6 18:56:08 2026
+
+@author: Prashanth
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importinng the data
+
+dataset=pd.read_csv(r"C:\Users\Prashanth\Desktop\Naresh_it\MachineLearning\6th - Navie Bayes\6th - Navie Bayes\Social_Network_Ads.csv")
+x=dataset.iloc[:,[2,3]].values
+y=dataset.iloc[:,-1].values
+
+# splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=0)
+
+# using feature scaling
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+x_train=sc.fit_transform(x_train)
+x_test=sc.transform(x_test)
+# training the naive bayes model onn the trainng set
+
+from sklearn.naive_bayes import GaussianNB
+classifier=GaussianNB()
+classifier.fit(x_train,y_train)
+
+# predicting the test set results
+
+y_pred=classifier.predict(x_test)
+
+# making the confusion matrix
+
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+print(cm)
+
+from sklearn.metrics import accuracy_score
+ac=accuracy_score(y_test,y_pred)
+print(ac)
+
+bias=classifier.score(x_train,y_train)
